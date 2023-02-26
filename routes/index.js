e094a62d5e8a1ca55e1ae039a11a12943c4799e5
ofer shelly <koko.akof10@outlook.com>
2023-02-26 13:15:21 +0200

    aaaaa

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express= require("express");
 const router = express.Router();
 
 router.get("/", async(req,res) => {
-  res.json({msg:"Api Work 200 12:05"});
+  res.json({msg:"Api Work 200 13:15"});
 })
 
 
